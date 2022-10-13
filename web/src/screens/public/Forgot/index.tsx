730b2cebaaef042f2ff028aa265dd33ceacd410f
Fares <fares.bourih@gmail.com>
2022-10-13 11:57:30 +0200

    fix(web): refacto import export of components folder

--- web/src/screens/public/Forgot/index.tsx
+++ web/src/screens/public/Forgot/index.tsx
@@ -1,13 +1,13 @@
 import React from "react";
-import C from "../../../components";
+import { Button, Title } from "../../../components";
 import { ROUTE_NAME } from "../../routes";
 
 export default function Forgot(props: any): React.ReactElement {
   return (
     <div>
-      <C.Title title="MOT DE PASSE OUBLIÉ" />
+      <Title title="MOT DE PASSE OUBLIÉ" />
 
-      <C.Button label="CONNEXION" onClick={() => props.navigation.navigate(ROUTE_NAME.LOGIN)} />
+      <Button label="CONNEXION" onClick={() => props.navigation.navigate(ROUTE_NAME.LOGIN)} />
     </div>
   );
 }
--- web/src/screens/public/Signup/index.tsx
+++ web/src/screens/public/Signup/index.tsx
@@ -1,13 +1,13 @@
 import React from "react";
-import C from "../../../components";
+import { Button, Title } from "../../../components";
 import { ROUTE_NAME } from "../../routes";
 
 export default function Signup(props: any): React.ReactElement {
   return (
     <div>
-      <C.Title title="INSCRIPTION" />
+      <Title title="INSCRIPTION" />
 
-      <C.Button label="CONNEXION" onClick={() => props.navigation.navigate(ROUTE_NAME.LOGIN)} />
+      <Button label="CONNEXION" onClick={() => props.navigation.navigate(ROUTE_NAME.LOGIN)} />
     </div>
   );
 }
